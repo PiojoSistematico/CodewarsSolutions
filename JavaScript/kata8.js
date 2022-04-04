e8cb55193c158ab14bee2fbeda178718d1beb5da
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-04 19:03:10 -0400

    added new problem

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -23,3 +23,12 @@ function basicOp(operation, value1, value2)
     default: return value1 / value2  
   }
 }
+
+//Build a function that returns an array of integers from n to 1 where n>0.
+const reverseSeq = n => {
+  result=[]
+  for(let i=n;i>0;i--){
+    result.push(i)
+  }
+  return result;
+};
