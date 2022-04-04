a76db1359ddf13304314d0ac2c3a86a9106485ac
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-03 20:23:08 -0400

    added one more solution

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -12,3 +12,14 @@ class SmallestIntegerFinder {
     return Math.min(...args)
   }
 }
+
+// basic math operation
+function basicOp(operation, value1, value2)
+{
+  switch (operation){
+    case "+": return value1 + value2
+    case "-": return value1 - value2
+    case "*": return value1 * value2
+    default: return value1 / value2  
+  }
+}
