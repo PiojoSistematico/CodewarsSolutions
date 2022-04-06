dfefca0a3377bdd9d97dd181c458a96155410d08
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-05 21:47:15 -0400

    added more problems

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -32,3 +32,18 @@ const reverseSeq = n => {
   }
   return result;
 };
+
+//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+//Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+function paperwork(n, m) {
+  if(n<0 || m<0){
+    return 0
+  } else{
+    return m*n
+  }
+}
+
+//Very simple, given an integer or a floating-point number, find its opposite.
+function opposite(number) {
+  return -number
+}
