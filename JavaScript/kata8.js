efb909ffd4cb7bef4304c6575c03d2702b7d6d39
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-08 20:11:24 -0400

    added more problems

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -86,8 +86,24 @@ function sum (numbers) {
 };
 
 //Implement a function which multiplies two numbers.
-
 function multiply(a,b){
   return a*b;
 }
 
+//You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+//Write a program that returns the girl's age (0-9) as an integer.
+//Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+function getAge(inputString){
+  return parseInt(inputString.charAt(0))
+}
+
+//If you can't sleep, just count sheep!! Task:
+//Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+const countSheep = function (num){
+  //your code here
+  result = ""
+  for(let i=1;i<=num;i++){
+    result += `${i} sheep...` 
+  }
+  return result
+}
