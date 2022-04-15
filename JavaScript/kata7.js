2220d9fe64533c8208d3d0a67455bba5f039f2f4
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-14 20:05:14 -0400

    added more problems

--- JavaScript/kata7.js
+++ JavaScript/kata7.js
@@ -41,3 +41,23 @@ function countBy(x, n) {
   }
   return z;
 }
+
+//Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+//For example: ["3:1", "2:2", "0:1", ...]
+//Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+//    if x > y: 3 points
+//    if x < y: 0 point
+//    if x = y: 1 point
+
+function points(games) {
+  sum = 0
+  for(let i=0;i<games.length;i++){
+    let game = games[i].split(":")
+    if(game[0]>game[1]){
+      sum += 3
+    } else if(game[0]==game[1]){
+      sum += 1
+    }
+  }
+  return sum
+}
