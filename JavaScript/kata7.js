b0fc1918302f7fcc01466b11b3ca43dac82f2fe7
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-19 20:48:33 -0400

    added more problems

--- JavaScript/kata7.js
+++ JavaScript/kata7.js
@@ -85,3 +85,21 @@ String.prototype.toJadenCase = function () {
 function openOrSenior(data){
   return data.map(elem => (elem[0]>=55 && elem[1]>7) ? "Senior" : "Open")
 }
+
+//Your task is to write function findSum.
+//Upto and including n, this function will return the sum of all multiples of 3 and 5.
+//For example:
+//findSum(5) should return 8 (3 + 5)
+//findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+function findSum(n) {
+  sum=0
+  for(let i=1;i<=n;i++){
+    if(i % 3 ===0){
+      sum+=i
+    } else if(i % 5 ===0){
+      sum+=i
+    }
+  }
+  return sum;
+}
