7e48a75aa995ce9be169b8245163f004fb257919
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-25 20:54:57 -0400

    added more problems

--- JavaScript/kata7.js
+++ JavaScript/kata7.js
@@ -142,3 +142,13 @@ function removeSmallest(numbers) {
 function getMiddle(s){
   return (s.length % 2 === 0) ? s.slice(s.length/2-1,s.length/2+1) : s.charAt(Math.floor(s.length/2))
 }
+
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+//Complete the method which accepts such an array, and returns that single different number.
+//The input array will always be valid! (odd-length >= 3)
+
+function stray(numbers) {
+  for (var i in numbers){
+     if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
+  }
+}
