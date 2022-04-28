a53f09d8225f4edf5501c99456852e87e4945172
Jorge Saa <piojo.sistematico@gmail.com>
2022-04-27 21:23:42 -0400

    added more problems

--- JavaScript/kata7.js
+++ JavaScript/kata7.js
@@ -152,3 +152,21 @@ function stray(numbers) {
      if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])){return numbers[i]}
   }
 }
+
+//Given the triangle of consecutive odd numbers:
+//
+//             1
+//          3     5
+//       7     9    11
+//   13    15    17    19
+//21    23    25    27    29
+//...
+//Calculate the sum of the numbers in the nth row of this triangle (starting at index 1)
+
+function rowSumOddNumbers(n) {
+  let sum=0;
+	for(let index=(n*(n-1)+1);index<n*(n+1); index+=2){
+    sum+=index;
+  }
+  return sum;
+}
