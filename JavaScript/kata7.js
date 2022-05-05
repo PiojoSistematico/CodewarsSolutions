7188f724e091e310276af4b87d0aab8ee0ffe248
Jorge Saa <piojo.sistematico@gmail.com>
2022-05-05 10:36:41 -0400

    added more problems

--- JavaScript/kata7.js
+++ JavaScript/kata7.js
@@ -288,3 +288,16 @@ function interest(P,r,n) {
   result[1]=Math.round(result[1])
   return result
 }
+
+//A square of squares
+//Given an integral number, determine if it's a square number:
+//In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+//The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+var isSquare = function(n){
+  if(n<0){
+    return false
+  } else{
+    return Math.sqrt(n) % 1 === 0
+  }
+}
