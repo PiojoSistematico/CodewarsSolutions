e9c9170d3089197df0fbad7527a5e8762f9501a8
Jorge Saa <piojo.sistematico@gmail.com>
2022-05-17 20:17:47 -0400

    added more problems

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -1,10 +1,6 @@
 //Some kata 8 solutions from Codewars
 
-//replace T for U in a string
-function DNAtoRNA(dna) {
-  // create a function which returns an RNA sequence from the given DNA sequence
-  return dna.replace(/T/g,"U")
-}
+
 
 //find the smallest elemnet in an array
 class SmallestIntegerFinder {
