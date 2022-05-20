b0dd472f309f1029c75dd1c659e5d2b6abc26d8f
Jorge Saa <piojo.sistematico@gmail.com>
2022-05-20 19:56:46 -0400

    added more problems

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -6,14 +6,7 @@
 
 
 
-//Build a function that returns an array of integers from n to 1 where n>0.
-const reverseSeq = n => {
-  result=[]
-  for(let i=n;i>0;i--){
-    result.push(i)
-  }
-  return result;
-};
+
 
 //Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 //Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
