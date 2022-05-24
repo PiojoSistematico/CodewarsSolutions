5f1224d2d22a6f82dfe7287f6f6087836583ddad
Jorge Saa <piojo.sistematico@gmail.com>
2022-05-23 20:24:25 -0400

    added more problems

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -2,22 +2,6 @@
 
 
 
-
-
-
-
-
-
-//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
-//Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
-function paperwork(n, m) {
-  if(n<0 || m<0){
-    return 0
-  } else{
-    return m*n
-  }
-}
-
 //Very simple, given an integer or a floating-point number, find its opposite.
 function opposite(number) {
   return -number
@@ -132,3 +116,9 @@ function maps(x){
 }
 
 
+/* Remove String Spaces
+Simple, remove the spaces from the string, then return the resultant string. */
+
+function noSpace(x){
+  return x.split(" ").join("")
+}
