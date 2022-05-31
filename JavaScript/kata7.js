2cd726f8ade09530662595d5477972c52470dd4d
Jorge Saa <piojo.sistematico@gmail.com>
2022-05-30 20:10:30 -0400

    added more problems

--- JavaScript/kata7.js
+++ JavaScript/kata7.js
@@ -1,18 +1,5 @@
 //Some kata 7 solutions from Codewars
 
-
-
-
-
-
-//Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
-//The binary number returned should be a string.
-//Examples:(Input1, Input2 --> Output (explanation)))
-
-function addBinary(a,b) {
-  return (a+b).toString(2);
-}
-
 //The museum of incredible dull things
 //The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
 //However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
