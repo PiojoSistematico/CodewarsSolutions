b6482117bbf05ef73d5cc69fe2b09d5d0bbe1cc6
Jorge Saa <piojo.sistematico@gmail.com>
2022-06-01 20:40:13 -0400

    added more problems

--- JavaScript/kata8.js
+++ JavaScript/kata8.js
@@ -9,10 +9,7 @@
 
 
 
-//Implement a function which multiplies two numbers.
-function multiply(a,b){
-  return a*b;
-}
+
 
 //You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
 //Write a program that returns the girl's age (0-9) as an integer.
